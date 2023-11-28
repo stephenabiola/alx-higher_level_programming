3cffabd52d2a3e882682efada4e3b0757fcf792c
stephenabiola <stephenabiola107@gmail.com>
2023-11-28 08:59:40 -0800

    task5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-for num in range(00, 100):
-    print("{:02d}".format(num), end='\n' if num == 99 else ", ")
+for i in range(99):
+    print("{:02d}".format(i), end=", ")
+print("{:02d}".format(99))
